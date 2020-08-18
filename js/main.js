1e68f85c4de367e5780465fc753ece0da49e059d
Nemanja <nemanja.mitic@msgconsulting.net>
2020-08-18 16:17:20 +0200

    add button for cart. Connect button to the cart section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,29 @@ var app = new Vue ({
 		product: 'Emegovitals Siberian Health',
 		image: 'images/green-omega.jpg',
 		inventory: 100,
-		details: ["Complex with omega-3 acids"," Vitamin E","Vitamin D"]
+		details: ["Complex with omega-3 acids"," Vitamin E","Vitamin D"],
+		variants:[
+			{
+				variantId: 001,
+				variantType: "green",
+				variantImage: 'images/green-omega.jpg'
+			},
+			{
+				variantId: 002,
+				variantType: "blue",
+				variantImage: 'images/blue-omega.jpg'
+			},
+			{
+				variantId: 003,
+				variantType: "yellow",
+				variantImage: 'images/yellow-omega.png'
+			}
+		],
+		cart:0
+	},
+	methods:{
+		addToCart: function(){
+			this.cart += 1
+		}
 	}
 })
