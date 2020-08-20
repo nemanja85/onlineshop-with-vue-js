99f79b37716433f0abb9cbdfa34920c9b236b21c
Nemanja <nemanja.mitic@msgconsulting.net>
2020-08-20 12:40:30 +0200

    Set the compomentproduct in the project. Refactoring the code for component product

--- js/main.js
+++ js/main.js
@@ -1,6 +1,53 @@
-var app = new Vue ({
-	el:'#app',
-	data: {
+Vue.component('product',{
+	template: `
+	<div class="product">
+				<nav class="navbar navbar-dark bg-dark">
+					<div class="cart">
+						<p class="float-right"><i class="fas fa-shopping-cart"></i> {{cart}}</p>
+					</div>
+				</nav>
+				<div class="container">
+					<div class="row mt-5">
+						<div class="col-sm-6 col-md-6 col-lg-4">
+							<div class="product-image">
+								<img :src="image" alt="" />
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-6 col-lg-8">
+							<div class="product-info">
+								<h2>{{ title }}</h2>
+								<p v-if="selectedVariant > 10">In Stock</p>
+								<p v-else-if="selectedVariant < 10 && selectedVariant > 0">Almost sold out!</p>
+								<p v-else>Out Stock</p>
+								
+								<ul>
+									<li v-for="detail in details">{{ detail }}</li>
+								</ul>
+								<div class="product-color">
+									<div v-for="(variant, index) in variants" 
+									 :key="variant.variantId" 
+									 :style="{backgroundColor: variant.variantType}"
+									 @mouseover="updateProduct(index)"
+									 class="color-box">
+									</div>
+								</div>
+								
+								<button @click="addToCart" 
+										:disabled="!inStock"
+										:class="{disabledButton: !inStock}"
+										class="btn btn-primary "> Add to Cart </button>
+								<button @click="removeFromCart" 
+										:disabled="!inStock" 
+										:class="{disabledButton: !inStock}"
+										class="btn btn-danger"> Remove from Cart </button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	`,
+	data(){
+		return {
 		product: 'Emegovitals',
 		brand: 'Siberian Health',
 		selectedVariant: 0,
@@ -26,6 +73,7 @@ var app = new Vue ({
 			}
 		],
 		cart:0
+		}
 	},
 	methods: {
 		addToCart: function(){
@@ -51,4 +99,9 @@ var app = new Vue ({
 			return this.variants[this.selectedVariant].variantQuantity
 		}
 	}
+})
+
+
+var app = new Vue ({
+	el:'#app'
 })
